6bbbf3edea40f9b93a74ce93b07738fc8599cfd3
ghkdrm123 <ghkdrm123@naver.com>
2021-10-28 01:51:09 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,26 +1,30 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 import random
 from email.mime.text import MIMEText
-from subprocess import Popen, PIPE 
+from email.mime.multipart import MIMEMultipart
+from subprocess import Popen, PIPE
 from datetime import datetime
+import time
+import base64
 
-giver = ["권희정", "박도연", "심영보", "권영재", "이상훈", "박예림", "황희재"] # 선물 주는 사람
-recipient = ["권희정", "박도연", "심영보", "권영재", "이상훈", "박예림", "황희재"] # 선물 받는 사람
+giver = ["손다미", "권희정", "박도연", "심영보", "권영재", "이상훈", "박예림", "황희재"] # 선물 주는 사람
+recipient = ["손다미","권희정", "박도연", "심영보", "권영재", "이상훈", "박예림", "황희재"] # 선물 받는 사람
 email = {
     "권희정" : "gmlwjd9405@gmail.com",
     "박도연" : "ehdus5958@naver.com",
     "심영보" : "sim4858@naver.com",
     "권영재" : "kyoje11@gmail.com",
     "이상훈" : "ttjs2008@gmail.com",
-    "박예림" : "p.yelimee@gmail.com",
-    "황희재" : "ghkdrm123@naver.com"
+    "박예림" : "park_yel@naver.com",
+    "황희재" : "ghkdrm123@naver.com",
+    "손다미" : "son.dami80@gmail.com"
     }
 
 trigger = 1
 
 while (trigger): # 주는사람과 받는사람이 같을 경우 셔플
-    print "shuffle"
+    #print("shuffle")
     for i in range(len(giver)):
         if (giver[i] == recipient[i]):
             random.shuffle(recipient)
@@ -32,13 +36,17 @@ while (trigger): # 주는사람과 받는사람이 같을 경우 셔플
 for i in range(len(giver)):
     now = datetime.now() # 현재 시간
 
+    print(giver[i] + "에게 보낸시간 = " + str(now))
+
     bodyTXT = "당신의 선물을 받을 사람은 '" + recipient[i] + "' 입니다.\n" + "발송시간 : " + str(now)
-    msg = MIMEText(bodyTXT) 
-    msg['Subject'] = '당신의 선물을 받을 사람은!?' 
-    msg['From'] = email.get(recipient[i])
+    #print(bodyTXT)
+    msg = MIMEText(bodyTXT)
+    msg['Subject'] = '당신의 선물을 받을 사람은!?'
+    msg['From'] = email.get("황희재")
     msg['To'] = email.get(giver[i])
 
-    # p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE) 
-    # p.communicate(msg.as_string())
+    p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE)
+    p.communicate(msg.as_string())
 
-    print giver[i] +"       "+email.get(giver[i])+"       "+str(now) +"       "+ recipient[i]
+    #print(giver[i] +"       "+email.get(giver[i])+"       "+str(now) +"       "+ recipient[i])
+    time.sleep(2)
